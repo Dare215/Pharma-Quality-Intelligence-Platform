6a72aef5eb0fd3b750a39d1240d86679d70ce63c
Dare215 <dariousbrown3@icloud.com>
2026-07-16 16:47:43 -0400

    Delete Python/clinical_sql_analysis.py

--- Python/clinical_sql_analysis.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-from sqlalchemy import create_engine
-
-engine=create_engine("postgresql://user:password@localhost/clinical_trials")
-print("Connect and execute advanced SQL queries here.")
